c4387f1e3bd5693d22042349f3a58cba33337410
hamster1198 <sk65771476@gmail.com>
2026-06-26 21:45:47 +0900

    [ADD] Character Initial Set

---
+++ Source/ProjectG/Private/Character/GProjectCharacter.cpp
@@ -0,0 +1,149 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Character/GProjectCharacter.h"
+
+#include "AbilitySystem/GProjectAbilitySystemComponent.h"
+#include "AbilitySystem/Abilities/GProjectGameplayAbility.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GProjectGameplayTags.h"
+#include "Player/GProjectPlayerState.h"
+
+AGProjectCharacter::AGProjectCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	bReplicates = true;
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+
+	GetCharacterMovement()->SetIsReplicated(true);
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 720.0f, 0.0f);
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->TargetArmLength = 1150.0f;
+	CameraBoom->SetRelativeRotation(FRotator(-58.0f, 0.0f, 0.0f));
+	CameraBoom->bUsePawnControlRotation = false;
+	CameraBoom->bInheritPitch = false;
+	CameraBoom->bInheritYaw = false;
+	CameraBoom->bInheritRoll = false;
+	CameraBoom->bDoCollisionTest = false;
+
+	TopDownCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("TopDownCamera"));
+	TopDownCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	TopDownCamera->bUsePawnControlRotation = false;
+	TopDownCamera->FieldOfView = 60.0f;
+}
+
+UAbilitySystemComponent* AGProjectCharacter::GetAbilitySystemComponent() const
+{
+	return GetGProjectAbilitySystemComponent();
+}
+
+UGProjectAbilitySystemComponent* AGProjectCharacter::GetGProjectAbilitySystemComponent() const
+{
+	const AGProjectPlayerState* GProjectPlayerState = GetPlayerState<AGProjectPlayerState>();
+	return GProjectPlayerState ? GProjectPlayerState->GetGProjectAbilitySystemComponent() : nullptr;
+}
+
+void AGProjectCharacter::HandleDeath()
+{
+	if (bDead)
+	{
+		return;
+	}
+
+	bDead = true;
+
+	if (UGProjectAbilitySystemComponent* ASC = GetGProjectAbilitySystemComponent())
+	{
+		ASC->AddLooseGameplayTag(GProjectGameplayTags::State_Dead);
+		ASC->CancelAllAbilities();
+	}
+
+	GetCharacterMovement()->StopMovementImmediately();
+	GetCharacterMovement()->DisableMovement();
+	GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+bool AGProjectCharacter::IsDead() const
+{
+	return bDead;
+}
+
+void AGProjectCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+
+	InitAbilityActorInfo();
+	AddCharacterAbilities();
+}
+
+void AGProjectCharacter::OnRep_PlayerState()
+{
+	Super::OnRep_PlayerState();
+
+	InitAbilityActorInfo();
+}
+
+void AGProjectCharacter::InitAbilityActorInfo()
+{
+	AGProjectPlayerState* GProjectPlayerState = GetPlayerState<AGProjectPlayerState>();
+	if (!GProjectPlayerState)
+	{
+		return;
+	}
+
+	UGProjectAbilitySystemComponent* ASC = GProjectPlayerState->GetGProjectAbilitySystemComponent();
+	if (!ASC)
+	{
+		return;
+	}
+
+	ASC->InitAbilityActorInfo(GProjectPlayerState, this);
+}
+
+void AGProjectCharacter::AddCharacterAbilities()
+{
+	if (!HasAuthority())
+	{
+		return;
+	}
+
+	UGProjectAbilitySystemComponent* ASC = GetGProjectAbilitySystemComponent();
+	if (!ASC)
+	{
+		return;
+	}
+
+	for (TSubclassOf<UGProjectGameplayAbility> AbilityClass : StartupAbilities)
+	{
+		if (!AbilityClass)
+		{
+			continue;
+		}
+
+		bool bAlreadyGranted = false;
+		for (const FGameplayAbilitySpec& AbilitySpec : ASC->GetActivatableAbilities())
+		{
+			if (AbilitySpec.Ability && AbilitySpec.Ability->GetClass() == AbilityClass)
+			{
+				bAlreadyGranted = true;
+				break;
+			}
+		}
+
+		if (bAlreadyGranted)
+		{
+			continue;
+		}
+
+		FGameplayAbilitySpec AbilitySpec(AbilityClass, 1);
+		ASC->GiveAbility(AbilitySpec);
+	}
+}
---
+++ Source/ProjectG/Public/Character/GProjectCharacter.h
@@ -0,0 +1,55 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "GameFramework/Character.h"
+#include "GProjectCharacter.generated.h"
+
+class UAbilitySystemComponent;
+class UCameraComponent;
+class UGProjectAbilitySystemComponent;
+class UGProjectGameplayAbility;
+class USpringArmComponent;
+
+UCLASS()
+class PROJECTG_API AGProjectCharacter : public ACharacter, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	AGProjectCharacter();
+
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	UGProjectAbilitySystemComponent* GetGProjectAbilitySystemComponent() const;
+
+	UFUNCTION(BlueprintCallable, Category = "Death")
+	virtual void HandleDeath();
+
+	UFUNCTION(BlueprintPure, Category = "Death")
+	bool IsDead() const;
+
+protected:
+	virtual void PossessedBy(AController* NewController) override;
+	virtual void OnRep_PlayerState() override;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Abilities")
+	TArray<TSubclassOf<UGProjectGameplayAbility>> StartupAbilities;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Death")
+	float RespawnDelay = 3.0f;
+
+private:
+	void InitAbilityActorInfo();
+	void AddCharacterAbilities();
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "Death", meta = (AllowPrivateAccess = "true"))
+	bool bDead = false;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USpringArmComponent> CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UCameraComponent> TopDownCamera;
+};
