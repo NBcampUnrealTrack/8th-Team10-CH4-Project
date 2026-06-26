48989995ca4756cd30b66bdc130b47f3a36ddda9
hamster1198 <sk65771476@gmail.com>
2026-06-26 21:45:47 +0900

    [ADD] Character Initial Set

---
+++ Source/ProjectG/Public/Game/GProjectGameMode.h
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GProjectGameMode.generated.h"
+
+UCLASS()
+class PROJECTG_API AGProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGProjectGameMode();
+};
---
+++ Source/ProjectG/Public/Game/GProjectGameState.h
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "GProjectGameState.generated.h"
+
+UCLASS()
+class PROJECTG_API AGProjectGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+};
