0fb8a067c9eef93fbe12ae5d9c47485783fd4a65
hamster1198 <sk65771476@gmail.com>
2026-06-26 21:49:10 +0900

    [ADD] Base Player State

---
+++ Source/ProjectG/Public/GProjectGameplayTags.h
@@ -0,0 +1,22 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace GProjectGameplayTags
+{
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Jump);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Dash);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Interact);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_BasicAttack);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_DashAttack);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_WeaponAttack);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_AccessorySkill);
+
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Dead);
+
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Data_Damage);
+}
