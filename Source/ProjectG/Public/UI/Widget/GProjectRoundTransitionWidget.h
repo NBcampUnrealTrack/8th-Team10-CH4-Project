e223fb4c74a9e8a052908b965ac12687c63a231e
Moon ChangWuk <moonchang0193@gmail.com>
2026-07-08 03:18:47 +0900

    [ADD] 라운드 전환 사이 띄울 UI

---
+++ Source/ProjectG/Public/UI/Widget/GProjectRoundTransitionWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Widget/GProjectUserWidget.h"
+#include "GProjectRoundTransitionWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTG_API UGProjectRoundTransitionWidget : public UGProjectUserWidget
+{
+	GENERATED_BODY()
+	
+};
