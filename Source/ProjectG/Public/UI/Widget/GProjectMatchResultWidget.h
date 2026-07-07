1c74a8256cab3f740e5481f6f7434c6bf9edf5be
Moon ChangWuk <moonchang0193@gmail.com>
2026-07-08 03:18:11 +0900

    [ADD] 최종 스코어 UI

---
+++ Source/ProjectG/Public/UI/Widget/GProjectMatchResultWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Widget/GProjectUserWidget.h"
+#include "GProjectMatchResultWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTG_API UGProjectMatchResultWidget : public UGProjectUserWidget
+{
+	GENERATED_BODY()
+	
+};
