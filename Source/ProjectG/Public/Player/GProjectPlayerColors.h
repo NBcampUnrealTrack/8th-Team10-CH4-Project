233a7e7eae872ec9dceb73fbaa0bb41545f1df26
Moon ChangWuk <moonchang0193@gmail.com>
2026-07-10 01:00:48 +0900

    [ADD] 색상 지정 위한 공용함수

---
+++ Source/ProjectG/Public/Player/GProjectPlayerColors.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+namespace GProjectPlayerColors
+{
+	inline FLinearColor GetColor(const int ColorIndex)
+	{
+		static const FLinearColor Colors[] =
+		{
+			FLinearColor(1.0f, 1.0f, 0.0f),
+			FLiearColor(1.0f, 1.0f, 1.0f),
+			FLinearColor(0.0f, 0.0f, 0.0f),
+			FLinearColor(0.0f, 1.0f, 1.0f)
+		};
+
+		if (ColorIndex < 0 || ColorIndex >= UE_ARRAY_COUNT(Colors))
+		{
+			return FLinearColor::White;
+		}
+		return Colors[ColorIndex];
+	}
+}
